fc6b551a83912454c9cf30d9fd408e0976fc4438
William Sherif <bill.sherif@gmail.com>
2024-02-03 11:24:07 -0800

    initial

---
+++ OpenGLApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OpenGLApp
+//
+//  Created by William Sherif on 2024-02-03.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ OpenGLApp/MyOpenGLView.h
@@ -0,0 +1,12 @@
+#ifndef MyOpenGLView_h
+#define MyOpenGLView_h
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyOpenGLView : NSOpenGLView {
+}
+
+- (void) drawRect: (NSRect) bounds;
+@end
+
+#endif /* MyOpenGLView_h */
