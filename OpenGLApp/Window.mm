1e835290db78f85233c2082b0be26925373a85d7
William Sherif <bill.sherif@gmail.com>
2024-10-04 22:04:16 -0400

    GCMouse callback using `dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);` to fix latency issue

---
+++ OpenGLApp/Window.mm
@@ -0,0 +1,11 @@
+#import "Window.h"
+
+@implementation Window
+
+- (void) noResponderFor:(SEL) eventSelector {
+  //printf( "window no responder for %s", NSStringFromSelector( eventSelector ).UTF8String );
+}
+
+
+
+@end
